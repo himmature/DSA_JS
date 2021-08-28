ed2d85fe1ea60a64bc0320b72f4ea6b31e092d28
himmature <hdyawalkar@gmail.com>
2021-08-28 23:13:01 +0530

    adding recursion binary search

--- SearchingAlgos/BinarySearchBasic.js
+++ SearchingAlgos/BinarySearchBasic.js
@@ -1,18 +1,25 @@
 const binarySearchBasic = (arr, value) => {
     let left = 0;
     let right = arr.length - 1;
-    while(left < right) {
-        let pivot = Math.ceil((left+right)/2);
+    while (left < right) {
+        let pivot = Math.ceil((left + right) / 2);
         if (arr[pivot] === value) {
             return pivot;
         } else if (arr[pivot] < value) {
             left = pivot;
-        } else if(arr[pivot] > value) {
+        } else if (arr[pivot] > value) {
             right = pivot;
         }
     }
     return -1;
 }
 
-const result = binarySearchBasic([2,4,7,8,22,26,34,50,57,69], 22);
+const binarySearchRecursion = (arr, value, left = 0, right = arr.length - 1) => {
+    let pivot = Math.ceil((left + right) / 2);
+    if (arr[pivot] === value) return pivot;
+    else if (arr[pivot] < value) return binarySearchBasic(arr, value, pivot + 1, right);
+    else return binarySearchBasic(arr, value, left, pivot - 1)
+}
+
+const result = binarySearchRecursion([2, 4, 7, 8, 22, 26, 34, 50, 57, 69], 4);
 console.log(result);
