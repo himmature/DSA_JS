e0bf6af5b01b313d1a27ce6955b70bf7ff847b15
himmature <hdyawalkar@gmail.com>
2021-09-26 18:06:55 +0530

    adding recursice DFS

--- BinarySearchTree/BInarySearchTree_recursive.js
+++ BinarySearchTree/BInarySearchTree_recursive.js
@@ -71,12 +71,68 @@ class BinarySearchTree {
         }        
         return traverseTree();
     }
+
+    traverse_DFS_preOrder() {
+        if(!this.root) return undefined;
+        let finalList = [];
+        let currentNode = this.root;
+        const traverseTree = (node) => {
+            finalList.push(node);
+            if(node.left) traverseTree(node.left);
+            if(node.right) traverseTree(node.right);
+        }
+        traverseTree(currentNode);
+        return finalList;
+    }
+
+    traverse_DFS_postOrder() {
+        if(!this.root) return undefined;
+        let finalList = [];
+        let currentNode = this.root;
+        const traverseTree = (node) => {
+            if(node.left) traverseTree(node.left);
+            if(node.right) traverseTree(node.right);
+            finalList.push(node);
+        }
+        traverseTree(currentNode);
+        return finalList;
+    }
+
+    traverse_DFS_inOrder() {
+        if(!this.root) return undefined;
+        let finalList = [];
+        let currentNode = this.root;
+        const traverseTree = (node) => {
+            if(node.left) traverseTree(node.left);
+            finalList.push(node);
+            if(node.right) traverseTree(node.right);
+        }
+        traverseTree(currentNode);
+        return finalList;
+    }
 }
 
 const tree = new BinarySearchTree();
 
-tree.insert(6);
 tree.insert(7);
+tree.insert(4);
+tree.insert(12);
 tree.insert(1);
-tree.insert(3);
-tree.insert(0);
+tree.insert(6);
+tree.insert(9);
+tree.insert(16);
+tree.insert(5);
+
+
+//     6
+//   1   7
+// 0   3
+
+// preOrder ----> node left right
+// 6,1,0,3,7
+
+// postOrder  ---> left right node
+// 0,3,1,7,6
+
+// inOrder ---> left node right
+// 0,1,3,6,7
